145c27d148fefde0dc39e177c5b62dfc21882eeb
Ephyy <vicente.ardiles@ug.uchile.cl>
2020-09-27 21:45:24 -0300

    Entrega Parcial 1

--- src/main/java/com/github/Ephyy/finalreality/model/character/player/Knight.java
+++ src/main/java/com/github/Ephyy/finalreality/model/character/player/Knight.java
@@ -6,17 +6,14 @@ import org.jetbrains.annotations.NotNull;
 import java.util.concurrent.BlockingQueue;
 
 /**
- * A class that holds all the information of a single common character in the game.
+ * A class that holds all the information of a single Knight character of the game.
  *
  * @author Ignacio Slater Muñoz.
  * @author Vicente Ardiles Silva.
  */
-public class Common extends AbstractPlayerCharacter {
+public class Knight extends AbstractPlayerCharacter implements ICommon {
 
-  /**
-   * Create a new common character with a name and common class
-   */
-  public Common(@NotNull String name, @NotNull BlockingQueue<ICharacter> turnsQueue,
+  public Knight(@NotNull String name, @NotNull BlockingQueue<ICharacter> turnsQueue,
                 CharacterClass characterClass) {
     super(name, turnsQueue, characterClass);
   }
